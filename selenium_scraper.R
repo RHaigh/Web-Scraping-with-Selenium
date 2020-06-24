b693255119ae842a21c406535f9859a34ec54ce2
Richard Haigh <45393669+RHaigh@users.noreply.github.com>
2020-06-24 13:49:53 +0100

    Update selenium_scraper.R
    updated due to html change in target website

--- selenium_scraper.R
+++ selenium_scraper.R
@@ -1,70 +1,17 @@
+# Import the necessary libraries
 library(openxlsx)
 library(stringr)
 library(purrr)
 library(RSelenium)
 library(readr)
+library(dplyr)
+library(glue)
+library(rvest)
 
-# Again, ensure you have aligned your browser to the driver. We will utilise Chrome in our search
-# As with our python version, we will find a list of locations and write our resulting tables to a csv file
+# We will use selenium to navigate to a fuel prices comparison website and collect data to compare prices across Scotland
+# This website has registration barriers and reactive javascript but we will show how selenium can easily bypass this
 
-# As we must apply a function to a list, we will define a custom function and apply it to each list element
-ScrapeFuel<-function(driver,PC){
-   
-  remDr <- driver[["client"]]
-  
-  #Open URL and navigate to the link
-  remDr$navigate("https://www.allstarcard.co.uk/tools/uk-fuel-prices/")
-  
-  # Allow pauses to account for slow reponse times
-  readrSys.sleep(3)
-  
-  # Type in post code once we have located the tex box by its css element 
-  PostCode<-remDr$findElement(using = 'css selector',  "#postcode")
-  # Send our string characters to this box
-  PostCode$sendKeysToElement(list(PC, key = "enter"))
-  Sys.sleep(3)
-  
-  # Click button to initiate the search
-  Button<-remDr$findElement(using = 'css selector',  '#find-my-nearest-station > button')
-  Button$clickElement()
-  
-  Sys.sleep(3)
-  # Grab resulting rendered data
-  t <- remDr$findElements(using = 'css selector', "#find-my-nearest-station > div > table")
-  t <- t$getElementText()
-  
-  return(t)
-  
-}
-
-# A handy custom function for isolating the last space character in a string
-FindLastSpace<-function(x){
-  LastSpace<-rev(gregexpr("\\ ", x)[[1]])[1]
-  return(LastSpace)
-}
-
-# A custom function for tidying the data into a readable format 
-Wrangle<-function(Scraped){
-  
-  Station <- as.character(Scraped)
-  Fuel<-strsplit(Station,"\n")
-  Fuel2<-data.frame(Station = unlist(Fuel), stringsAsFactors = FALSE)
-  FuelSplit<-do.call(rbind,strsplit(Fuel2$Station," miles "))
-  Prices<-do.call(rbind,strsplit(FuelSplit[,2],"\ "))
-  Fuel2$Petrol <- Prices[,1]
-  Fuel2$Diesel <- Prices[,2]
-  Location<-FuelSplit[,1]
-  Split<-map(Location,~FindLastSpace(.x))
-  Fuel2 <- Fuel2[-c(1:7),] 
-  Fuel2$Date <- Sys.Date()
-  Fuel2$Station <- gsub("[0-9].*", "", Fuel2$Station)
-  return(Fuel2)
-  
-}
-
-# Now we have defined our custom functions, we apply them to each item in our list
-
-# Open browser using selenium
+# Open browser using selenium after installing the necessary software
 driver<- rsDriver(
   browser = "firefox", geckover = "latest",
   extraCapabilities = list(
@@ -74,25 +21,87 @@ driver<- rsDriver(
   )
 )
 
-# Scrape data
+# Note if you receive a selenium error at this point, you most likely have to update your firefox gecko driver
+# Run binman::list_versions("geckodriver") to see available drivers
+# Use following code to change RSelenium default version used to compatible version
+# driver<- rsDriver(browser=c("firefox"), geckover="8.0.3904.70") or simply put 'latest' if your drivers are all up to date
 
-# CHANGE THE DESTINATIONS YOU WISH TO ENTER INTO THE SEARCH BAR HERE
-location_list <- c("Dunrossness", "Orkney", "Lochbroom", "Aberfeldy", "Kinnaird", 
+# This is a headless version but, if you would ike to see what your coe is doing, you can use:
+driver<- rsDriver(
+  browser = "firefox", geckover = "latest"
+)
+
+remDr <- driver[["client"]]
+
+# Open URL
+remDr$navigate("https://www.allstarcard.co.uk/tools/uk-fuel-prices/")
+
+# Allow a pause for the html to load
+Sys.sleep(3)
+
+# Now we can see that we must register in order to proceed but testing shows that the user simply needs to submit details, not reply to an email registration
+# So we will identify the input boxes using css selector and enter faux registration details
+Name<-remDr$findElement(using = 'css selector',  "#Name")
+Name$sendKeysToElement(list('MyName'))
+
+# Generate a random email address using a random string gnerator
+random_string_generator <- function(n = 5000) {
+  a <- do.call(paste0, replicate(5, sample(LETTERS, n, TRUE), FALSE))
+  paste0(a, sprintf("%04d", sample(9999, n, TRUE)), sample(LETTERS, n, TRUE))
+}
+
+email <- random_string_generator(1)
+
+Email<-remDr$findElement(using = 'css selector',  "#Email")
+Email$sendKeysToElement(list(glue('{email}@gmal.com'), key = "enter")) # Hit enter and submit our details 
+
+# We can now see that the webpage is ready for us to submit our locations
+# We want to look at a list of locations so will create a custom function that will enter in each location and collect the resulting table
+
+ScrapeFuel<-function(driver, location) {
+  
+  remDr <- driver[["client"]]
+  
+  #Open URL
+  remDr$navigate("https://www.allstarcard.co.uk/tools/uk-fuel-prices/")
+  
+  # Type in location to input text box
+  searchBox <- remDr$findElement(using = 'css selector',  "#NearbySitesLocation")
+  searchBox$sendKeysToElement(list(location, key = "enter"))
+  
+  # Get our data by passing the url object to rvest and collecting using html_nodes 
+  html <- remDr$getPageSource()[[1]]
+  
+  table <- read_html(html) %>%
+    html_nodes("table.table.table-bordered.results-table") %>% # we find the table class name using web developer tools in chrome
+    .[[1]] %>%
+    html_table(fill=T) #html_table handily converts data packets into data frame format
+  
+  # Add additional columns and tidy away empty ones
+  table$Date <- Sys.Date()
+  table$Location <- location
+  table$Map <- NULL
+  table$Petrol <- NULL
+  table$Diesel <- NULL
+  
+  return(table)
+  
+}
+
+# So now we have our custom function, let's put together a list of locations we want it to search for
+location_list <- c("Dunrossness", "Orkney", "Aberfeldy", "Kinnaird", 
                    "Springside", "Moffat", "Linton", "Ibrox", "Edinburgh", "Glasgow", 
                    "Dundee", "Aberdeen", "Stranrae", "Girvan", "Fife")
 
-# Apply our custom scraping function to each element and create a list of dataframe results using lapply
+# and using assign and lapply we apply this custom function to every location and put the resulting df into a list
 fuel_prices_list <- lapply(location_list, function(i) {
   assign(i, ScrapeFuel(driver, i))
-})
-
-# Tidy data and create a list of tidy dataframe results 
-fuel_prices_tidy <- lapply(fuel_prices_list, function(i) {
-  assign(as.character(i), Wrangle(i))
 })
 
 # Use to close browser
 driver$server$stop()
 
-# Save the resulting price tables as csv files - here is an example of the syntax
-write.csv("fuel_prices_location1", fuel_prices_tidy[[1]])
+# Bundle all our dataframes together with rbind and we have our comprehensive list of fuel prices across Scotland
+fuel_prices_current <- bind_rows(fuel_prices_list)
+
+# END OF SCRIPT
