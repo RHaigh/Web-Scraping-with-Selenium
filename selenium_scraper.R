2fbe62114a45799288bcd59d75970cff3f934855
Richard Haigh <45393669+RHaigh@users.noreply.github.com>
2020-06-25 10:37:41 +0100

    Update selenium_scraper.R

--- selenium_scraper.R
+++ selenium_scraper.R
@@ -12,7 +12,7 @@ library(rvest)
 # This website has registration barriers and reactive javascript but we will show how selenium can easily bypass this
 
 # Open browser using selenium after installing the necessary software
-driver<- rsDriver(
+driver <- rsDriver(
   browser = "firefox", geckover = "latest",
   extraCapabilities = list(
     "moz:firefoxOptions" = list(
@@ -27,7 +27,7 @@ driver<- rsDriver(
 # driver <- rsDriver(browser=c("firefox"), geckover="8.0.3904.70") or simply put 'latest' if your drivers are all up to date
 
 # This is a headless version but, if you would like to see what your code is doing, you can use:
-driver<- rsDriver(
+driver <- rsDriver(
   browser = "firefox", geckover = "latest"
 )
 
@@ -41,10 +41,11 @@ Sys.sleep(3)
 
 # Now we can see that we must register in order to proceed but testing shows that the user simply needs to submit details, not reply to an email registration
 # So we will identify the input boxes using css selector and enter faux registration details
-Name<-remDr$findElement(using = 'css selector',  "#Name")
+Name <- remDr$findElement(using = 'css selector',  "#Name")
 Name$sendKeysToElement(list('MyName'))
 
-# Generate a random email address using a random string generator
+# Now for each email registration, only a set number of searches are allowed so we will need to enter a new false email with each scrape
+# Generate a random email address using a random string generator and string interpolation
 random_string_generator <- function(n = 5000) {
   a <- do.call(paste0, replicate(5, sample(LETTERS, n, TRUE), FALSE))
   paste0(a, sprintf("%04d", sample(9999, n, TRUE)), sample(LETTERS, n, TRUE))
@@ -52,17 +53,20 @@ random_string_generator <- function(n = 5000) {
 
 email <- random_string_generator(1)
 
-Email<-remDr$findElement(using = 'css selector',  "#Email")
+Email <- remDr$findElement(using = 'css selector',  "#Email")
 Email$sendKeysToElement(list(glue('{email}@gmal.com'), key = "enter")) # Hit enter and submit our details 
 
-# We can now see that the webpage is ready for us to submit our locations
-# We want to look at a list of locations so will create a custom function that will enter in each location and collect the resulting table
+# We can now see that the webpage is ready for us to submit our locations. Again, find the necessary box using chrome css selector
+searchBox <- remDr$findElement(using = 'css selector',  "#NearbySitesLocation")
+  searchBox$sendKeysToElement(list(location, key = "enter"))
 
-ScrapeFuel<-function(driver, location) {
+# But we want to look at a list of locations so will create a custom function that will enter in each location and collect the resulting table
+
+ScrapeFuel <- function(driver, location) {
   
   remDr <- driver[["client"]]
   
-  #Open URL
+  # Open URL again - necessary as there is no search box in the table render page
   remDr$navigate("https://www.allstarcard.co.uk/tools/uk-fuel-prices/")
   
   # Type in location to input text box
@@ -103,5 +107,10 @@ driver$server$stop()
 
 # Bundle all our dataframes together with rbind and we have our comprehensive list of fuel prices across Scotland
 fuel_prices_current <- bind_rows(fuel_prices_list)
+
+# So the complete workflow will be:
+# 1. Naviagate to url and create registration details
+# 2. Cycle through list of location searches using custom function
+# 3. Collect and collate results into a single dataframe
 
 # END OF SCRIPT
