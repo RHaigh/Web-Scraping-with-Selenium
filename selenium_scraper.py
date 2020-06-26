8fbc68ea47ce3822fab5490253429f486634897b
Richard Haigh <45393669+RHaigh@users.noreply.github.com>
2020-06-26 12:42:00 +0100

    Update selenium_scraper.py
    Updated for change in source html

--- selenium_scraper.py
+++ selenium_scraper.py
@@ -1,52 +1,95 @@
 import time
+import pandas as pd
 import csv
+import random
+import string
 from bs4 import BeautifulSoup
-from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+
+# As with our R script, we will look to collect a range of fuel prices from a price comparison website into a single dataframe
 
 # As my chosen browser is chrome 79, I must match this to chromedriver 79.0.3495
+driver = webdriver.Chrome(executable_path=r"/Path/to/chromedriver")
+
+#  We can also initialise browser in headless mode to speed up the process and save on memory
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+driver = webdriver.Chrome(options=chrome_options, executable_path=r"/Users/richardhaigh/Downloads/chromedriver")
+
+# Custom function we will use to imitate slow typing and fool anti-bot software
+def slow_typing(element, text):
+    for character in text:
+        element.send_keys(character)
+        time.sleep(0.3)
+
+# When first visiting the website, we will be asked to register with a name and email address
+# As this anti-scraping measure does not require email address confirmation, we will enter in false details to proceed
+driver.get("https://www.allstarcard.co.uk/tools/uk-fuel-prices/")
+
+# We will find the 'Name' text box and enter in the following keys
+element = driver.find_element_by_css_selector("#Name")
+element.send_keys('MyName')
+
+# Next we find the 'Email' text box. As each email only allows a limited number of searches, we will need to create a new one everytime
+# We will generate a random string and enter it in email format using string interpolation
+def randomString(stringLength=8):
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(stringLength))
+
+email = randomString(8)
+
+element = driver.find_element_by_css_selector("#Email")
+element.send_keys(f'{email}@gmail.com')
+# We now send the return key from the keyboard to initiate our search
+element.send_keys(Keys.RETURN)
+# Allow a brief pause for the web page to load
+time.sleep(5)
+
+# We are now presented with a new selection box where we can enter a location and view local fuel prices
+element = driver.find_element_by_css_selector("#NearbySitesLocation")
+element.send_keys('Edinburgh')
+element.send_keys(Keys.RETURN)
+
+# A new html table is rendered with our results. We can use BeautifulSoup to iterate over this table and collate a dataframe
+soup = BeautifulSoup(driver.page_source, 'lxml')
+table = soup.find_all('table')[2] # There are several tables within the html so we must grab the correct one
+
+# Handily, pandas contains a function for converting bs4 objects into pd dataframes
+df = pd.read_html(str(table))[0]
+
+# We will now remove unnecessary blank columns and add one to show the date of the search and area
+df = df.drop(['Map', 'Diesel', 'Petrol'], axis = 1)
+df['Search Date'] = pd.to_datetime('today')
+df['Area'] = 'Ediburgh'
+
+# This is all we need to collect a dataframe of our fuel prices
+# If we wished to search a list of locations and collate the results, we would use a custom function to loop through our list
 
 # Enter in our chosen list of locations to search
 location_list = ("Edinburgh", "Orkney", "Glasgow", "Aberdeen", "Girvan", "Kinnaird",
                    "Stranrae", "Moffat", "Aberdeen", "Ibrox")
 
-# Define direct path to the chromedriver.exe file
-driver = webdriver.Chrome(executable_path=r"/Path/to/chromedriver")
-
-# We could write to a table or dict object but for the practicality, we will write our table to a csv file
-
-# As we have a list of locations, we will utilise a for loop
+list_of_dataframes = []
 for i in location_list:
-    filename = (i + "_fuel_prices.csv")
-    csv_writer = csv.writer(open(filename, 'w'))
-
-    # Driver.get navigates to a given url
+    # The registration details are only required once per active session so we need not include this in every loop as it will result in errors
     driver.get("https://www.allstarcard.co.uk/tools/uk-fuel-prices/")
-    # Open up developer mode in Chrome or inspect element in Safari to identify the html tag required. Here we are looking for the text entry box
-    element = driver.find_element_by_css_selector("#postcode")
-    # Now we have found the text box, we send our string characters
+
+    element = driver.find_element_by_css_selector("#NearbySitesLocation")
     element.send_keys(i)
-    # Followed by the search button. We will find both by their css tag. We could also use class or id finder functions
-    element = driver.find_element_by_css_selector("#find-my-nearest-station > button")
-    # We now send the return key from the keyboard to initiate our search
     element.send_keys(Keys.RETURN)
-    time.sleep(5)
 
-    # Hand over Selenium results to BS which is far better suited to hunting through our scraped results
     soup = BeautifulSoup(driver.page_source, 'lxml')
-    # Identify results table
-    table = soup.find_all('table')[2]
-    # Select all entries within this table with the html tag <tr> as our desired data
-    rows = table.select('tbody > tr')
-    # Identify rows with tag <th> to be used as headers in our csv table
-    header = [th.text.rstrip() for th in table.find_all('th')]
+    table = soup.find_all('table')[2]  # There are several tables within the html so we must grab the correct one
 
-    # Write header as first row then iterate over body results
-    with open(filename, 'w') as csv_file:
-        writer = csv.writer(csv_file)
-        writer.writerow(header)
-        # Iterate over subsequent rows below each header with the tag <td> to write our table
-        for row in rows[0:]:
-            data = [th.text.rstrip() for th in row.find_all('td')]
-            writer.writerow(data)
+    df = pd.read_html(str(table))[0]
+    df = df.drop(['Map', 'Diesel', 'Petrol'], axis=1)
+    df['Search Date'] = pd.to_datetime('today')
+    df['Area'] = i
+
+    list_of_dataframes.append(df)
+
+final_table = pd.concat(list_of_dataframes)
+
 
